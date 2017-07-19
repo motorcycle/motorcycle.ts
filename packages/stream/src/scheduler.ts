4ea8aa902280ad76b0e00a84f66efcdb5f93a61d
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-07-19 13:32:38 -0400

    docs(scheduler): add documentation to scheduler

--- packages/stream/src/scheduler.ts
+++ packages/stream/src/scheduler.ts
@@ -1,4 +1,24 @@
 import { Scheduler } from '@motorcycle/types'
 import { newDefaultScheduler } from '@most/scheduler'
 
+/**
+ * A shared instance of the default scheduler from @most/scheduler
+ * 
+ * @name scheduler (Scheduler)
+ * @example
+ * import { scheduler, now } from '@motorcycle/stream'
+ * 
+ * const stream = now(1)
+ * 
+ * const sink = {
+ *   event(time: number, value: number) { ... },
+ *   error(time: number, err: Error) { ... },
+ *   end(time: number) { ... }
+ * }
+ * 
+ * const disposable = stream.run(sink, scheduler)
+ * 
+ * // later 
+ * disposable.dispose()
+ */
 export const scheduler: Scheduler = newDefaultScheduler()
