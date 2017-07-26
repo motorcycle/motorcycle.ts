e0044817a15490f766d595a98e6da763fd2e301e
Frederik Krautwald <fkrautwald@gmail.com>
2017-07-26 21:28:59 +0200

    implement elements for EventDelegationDomSource
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

--- packages/dom/src/DomSources/EventDelgationDomSource.test.ts
+++ packages/dom/src/DomSources/EventDelgationDomSource.test.ts
@@ -1,8 +1,9 @@
 import { CssSelector, DomSource } from '../'
 import { Test, describe, given, it } from '@typed/test'
+import { empty, mergeArray, now } from '@motorcycle/stream'
 
 import { EventDelegationDomSource } from './'
-import { empty } from '@motorcycle/stream'
+import { collectEventsFor } from '@motorcycle/test'
 import { length } from '167'
 
 export const test: Test = describe(`EventDelegationDomSource`, [
@@ -45,6 +46,82 @@ export const methodsTest: Test = describe(`EventDelegationDomSource methods`, [
 
         equal(1, length(cssSelectors))
         equal(existingSelector, cssSelectors[0])
+      }),
+    ]),
+  ]),
+
+  describe(`elements`, [
+    given(`a DomSource with a stream of element and empty CSS selectors`, [
+      it(`returns a stream of elements with only the root element`, ({ equal }) => {
+        const rootEl = document.createElement('div')
+        const sut: DomSource = new EventDelegationDomSource(now(rootEl), [])
+        const elements$ = sut.elements()
+
+        return collectEventsFor(1, elements$).then(equal([[rootEl]]))
+      }),
+    ]),
+
+    given(`a DomSource with a stream of element with queried CSS selectors`, [
+      it(`returns a stream of elements with only the most specific elements`, ({ equal }) => {
+        const rootEl = document.createElement('div')
+        const childEl = document.createElement('div')
+
+        rootEl.classList.add(`foo`)
+        childEl.classList.add(`foo`)
+        rootEl.appendChild(childEl)
+
+        const sut: DomSource = new EventDelegationDomSource(now(rootEl), [`.foo`])
+        const elements$ = sut.elements()
+
+        return collectEventsFor(1, elements$).then(equal([[rootEl, childEl]]))
+      }),
+    ]),
+
+    given(`a DomSource with a stream of element with children matching CSS selectors`, [
+      it(`returns a stream of elements with only the most specific elements`, ({ equal }) => {
+        const rootEl = document.createElement('div')
+        const childEl = document.createElement('div')
+
+        childEl.classList.add(`foo`)
+        rootEl.appendChild(childEl)
+
+        const sut: DomSource = new EventDelegationDomSource(now(rootEl), [`.foo`])
+        const elements$ = sut.elements()
+
+        return collectEventsFor(1, elements$).then(equal([[childEl]]))
+      }),
+    ]),
+
+    given(`a DomSource with a stream of element with non-matching CSS selectors`, [
+      it(`returns a stream of elements with only the most specific elements`, ({ equal }) => {
+        const rootEl = document.createElement('div')
+        const childEl = document.createElement('div')
+
+        childEl.classList.add(`foo`)
+        rootEl.appendChild(childEl)
+
+        const sut: DomSource = new EventDelegationDomSource(now(rootEl), [`.bar`])
+        const elements$ = sut.elements()
+
+        return collectEventsFor(1, elements$).then(equal([]))
+      }),
+    ]),
+
+    given(`a DomSource with a repeating stream of element of matching CSS selectors`, [
+      it(`returns a repeating stream of elements`, ({ equal }) => {
+        const rootEl = document.createElement('div')
+        const childEl = document.createElement('div')
+
+        childEl.classList.add(`foo`)
+        rootEl.appendChild(childEl)
+
+        const sut: DomSource = new EventDelegationDomSource(
+          mergeArray([now(rootEl), now(rootEl)]),
+          [`.foo`]
+        )
+        const elements$ = sut.elements()
+
+        return collectEventsFor(1, elements$).then(equal([[childEl], [childEl]]))
       }),
     ]),
   ]),
