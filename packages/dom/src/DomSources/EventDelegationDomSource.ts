c396a523ccd53d78aeffc17c61455ec2f040c322
Frederik Krautwald <fkrautwald@gmail.com>
2017-07-26 21:28:59 +0200

    implement elements for EventDelegationDomSource
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

--- packages/dom/src/DomSources/EventDelegationDomSource.ts
+++ packages/dom/src/DomSources/EventDelegationDomSource.ts
@@ -1,17 +1,20 @@
 import { CssSelector, DomSource } from '../'
-import { append, equals } from '167'
+import { append, copy, equals, gt, join, length, pipe, prepend } from '167'
+import { empty, filter, map } from '@motorcycle/stream'
 
 import { Stream } from '@motorcycle/types'
-import { empty } from '@motorcycle/stream'
 
 const ROOT_CSS_SELECTOR: CssSelector = `:root`
+
+const CSS_SELECTOR_JOINT = ` `
 
 export class EventDelegationDomSource implements DomSource {
   private _cssSelectors: ReadonlyArray<CssSelector>
 
-  constructor(element$: Stream<Element>, cssSelectors: ReadonlyArray<CssSelector>) {
-    Function.prototype(element$)
+  private _element$: Stream<Element>
 
+  constructor(element$: Stream<Element>, cssSelectors: ReadonlyArray<CssSelector>) {
+    this._element$ = element$
     this._cssSelectors = cssSelectors
   }
 
@@ -22,7 +25,14 @@ export class EventDelegationDomSource implements DomSource {
   }
 
   public elements<El extends Element = Element>(): Stream<ReadonlyArray<El>> {
-    return empty()
+    const cssSelectors = this._cssSelectors
+
+    if (equals(0, length(cssSelectors))) return map(Array, this._element$)
+
+    const hasElements = pipe(length, gt(0))
+    const cssSelector = join(CSS_SELECTOR_JOINT, cssSelectors)
+
+    return filter(hasElements, map(findMatchingElements(cssSelector), this._element$))
   }
 
   public events<Ev extends Event = Event>(): Stream<Ev> {
@@ -31,5 +41,15 @@ export class EventDelegationDomSource implements DomSource {
 
   public cssSelectors(): ReadonlyArray<CssSelector> {
     return this._cssSelectors
+  }
+}
+
+function findMatchingElements<El extends Element = Element>(cssSelector: CssSelector) {
+  return function(element: El): ReadonlyArray<El> {
+    const nodes = copy(element.querySelectorAll(cssSelector) as NodeListOf<El>)
+
+    if (element.matches(cssSelector)) return prepend(element, nodes)
+
+    return nodes
   }
 }
--- packages/dom/src/types/DomSource.ts
+++ packages/dom/src/types/DomSource.ts
@@ -1,9 +1,45 @@
 import { StandardEvents } from './Events'
 import { Stream } from '@motorcycle/types'
 
+/**
+ * A DOM source interface for objects to declaratively query the DOM.
+ *
+ * @name DomSource
+ *
+ * @example
+ * interface DomSource {
+ *   query(cssSelector: CssSelector): DomSource
+ *   elements<El extends Element = Element>(): Stream<ReadonlyArray<El>>
+ *   events<Ev extends Event = Event>(eventType: StandardEvents, options?: AddEventListenerOptions): Stream<Ev>
+ *   cssSelectors(): ReadonlyArray<CssSelector>
+ * }
+ *
+ * @type
+ */
 export interface DomSource {
+  /**
+   * Queries for elements and events for a specified CSS selector.
+   *
+   * @name DomSource.query(cssSelector: CssSelector): DomSource
+   *
+   * @example
+   * const queriedDomSource = domSource.query(`.myCssSelector`)
+   */
   query(cssSelector: CssSelector): DomSource
+
+  /**
+   * Retrieves a stream of a list of elements matching previous queries.
+   *
+   * NOTE: Elements will emit every single time the DOM is updated.
+   *
+   * @name DomSource.elements<El extends Element = Element>(): Stream<ReadonlyArray<El>>
+   *
+   * @example
+   * const queriedDomSource = domSource.query(`.myCssSelector`)
+   * const elements$ = queriedDomSource.elements()
+   */
   elements<El extends Element = Element>(): Stream<ReadonlyArray<El>>
+
   events<Ev extends Event = Event>(
     eventType: StandardEvents,
     options?: AddEventListenerOptions
