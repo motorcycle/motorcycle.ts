6c6396ce04d9edca39438f3884352b525bf723a3
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-08-16 14:58:03 -0400

    docs(stream): document sampleWith

--- packages/stream/src/combinators/sampleWith.ts
+++ packages/stream/src/combinators/sampleWith.ts
@@ -1,6 +1,24 @@
 import { Stream } from '@motorcycle/types'
 import { sample } from './sample'
 
+/**
+ * Given each event occurrence from a sampler stream takes the latest value from 
+ * the given stream. 
+ * 
+ * @name sampleWith<A>(sampler: Stream<any>, stream: Stream<A>): Stream<A>
+ * @example
+ * import { sampleWith } from '@motorcycle/stream'
+ * 
+ * function submit(dom: DomSource): Stream<string> {
+ *   const button = query('button', dom)
+ *   const input = query('input', dom)
+ * 
+ *   const click$ = events('click', button)
+ *   const value$ = map(ev => ev.target.value, events('input', input))
+ * 
+ *   return sampleWith(click$, value$)
+ * }
+ */
 export const sampleWith = sample(takeRight) as SampleWith
 
 export interface SampleWith {
