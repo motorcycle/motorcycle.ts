44f85e9088fd3f8bb9197c2e85169f281d5dbffa
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-08-23 18:58:46 -0400

    feat(dom): fakeDomSource

--- packages/dom/src/index.ts
+++ packages/dom/src/index.ts
@@ -1,5 +1,6 @@
 export * from './createDocumentDomSource'
 export * from './createDomSource'
+export * from './fakeDomSource'
 export * from './types'
 
 export { DocumentDomSource } from './DomSources'
