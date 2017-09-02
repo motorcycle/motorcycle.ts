406dc89cb2425bd90d1ba447c5d23847af786f6f
Frederik Krautwald <fkrautwald@gmail.com>
2017-09-02 17:49:06 +0200

    curry switchMap
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

--- packages/stream/src/combinators/switchMap.ts
+++ packages/stream/src/combinators/switchMap.ts
@@ -1,4 +1,5 @@
 import { Stream } from '@motorcycle/types'
+import { curry2 } from '167'
 import { map } from './map'
 import { switchLatest } from './switchLatest'
 
@@ -19,6 +20,19 @@ import { switchLatest } from './switchLatest'
  *
  * observe(console.log, s) // 3
  */
-export function switchMap<A, B = A>(f: (a: A) => Stream<B>, s: Stream<A>): Stream<B> {
+export const switchMap: SwitchMapArity2 = curry2(function switchMap<A, B = A>(
+  f: (a: A) => Stream<B>,
+  s: Stream<A>
+): Stream<B> {
   return switchLatest(map(f, s))
+})
+
+export interface SwitchMapArity2 {
+  <A, B = A>(f: (a: A) => Stream<B>, s: Stream<A>): Stream<B>
+
+  <A, B = A>(f: (a: A) => Stream<B>): SwitchMapArity1<A, B>
+}
+
+export interface SwitchMapArity1<A, B = A> {
+  (s: Stream<A>): Stream<B>
 }
