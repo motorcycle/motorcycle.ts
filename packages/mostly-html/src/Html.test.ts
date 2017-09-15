3380e5cc67ee07e6979016ceb27efbf7324f7c99
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-09-15 13:14:57 -0400

    fix: ensure tests can pass on travis

--- packages/mostly-html/src/Html.test.ts
+++ packages/mostly-html/src/Html.test.ts
@@ -1,10 +1,10 @@
+import { DomSinks, DomSources, button, div, h1 } from '@motorcycle/mostly-dom'
 import { Test, describe, given, it } from '@typed/test'
+import { constant, map, merge, observe, scan } from '@motorcycle/stream'
+import { events, query } from '@motorcycle/dom'
 
-import { DomSinks } from '@motorcycle/mostly-dom'
 import { Html } from './Html'
 import { HtmlSources } from './types'
-import { UI } from '../../../examples/counter/src/UI'
-import { observe } from '@motorcycle/stream'
 import { run } from '@motorcycle/test'
 
 export const test: Test = describe(`Html`, [
@@ -21,3 +21,29 @@ export const test: Test = describe(`Html`, [
     }),
   ]),
 ])
+
+const click = events('click')
+const INCREMENT_ID = 'increment'
+const DECREMENT_ID = 'decrement'
+const INCREMENT_SELECTOR = `#${INCREMENT_ID}`
+const DECREMENT_SELECTOR = `#${DECREMENT_ID}`
+
+const add = (x: number, y: number) => x + y
+
+function UI(sources: DomSources): DomSinks {
+  const { dom } = sources
+  const increment$ = constant(+1, click(query<Element>(INCREMENT_SELECTOR, dom)))
+  const decrement$ = constant(-1, click(query<Element>(DECREMENT_SELECTOR, dom)))
+  const amount$ = scan(add, 0, merge(increment$, decrement$))
+  const view$ = map(view, amount$)
+
+  return { view$ }
+}
+
+function view(amount: number) {
+  return div([
+    h1(`Count: ${amount}`),
+    button({ id: INCREMENT_ID }, `Increment +`),
+    button({ id: DECREMENT_ID }, `Decrement -`),
+  ])
+}
