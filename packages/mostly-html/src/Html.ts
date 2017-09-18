40073b565b6fab2d293e2eb799ec7915d7b41e8c
Frederik Krautwald <fkrautwald@gmail.com>
2017-09-18 12:39:43 +0200

    Correct typos in documentation
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

--- packages/mostly-html/src/Html.ts
+++ packages/mostly-html/src/Html.ts
@@ -5,16 +5,16 @@ import { HtmlDomSource } from './HtmlDomSource'
 import { toHtml } from 'mostly-html'
 
 /**
- * Renders mostly-dom VNodes into HTML for server-sider rendering.
+ * Renders mostly-dom VNodes into HTML for server-side rendering.
  * @name Html<A = Element, B = Event>(sinks: HtmlSinks): HtmlSources<A, B>
  * @example
  * import { run } from '@motorcycle/run'
  * import { Html, HtmlSources, HtmlSinks } from '@motorcycle/mostly-html'
  * import { UI } from './UI'
  * import { observe } from '@motorcycle/stream'
- * 
+ *
  * const { sources: { html$ } } = run<HtmlSources, HtmlSinks>(UI, Html)
- * 
+ *
  * observe((html: string) => { /* Do something with html *\/ }, html$)
  */
 export function Html<A = Element, B = Event>(sinks: HtmlSinks): HtmlSources<A, B> {
