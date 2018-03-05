edf0f372ad28d4fc9bc1219fd013235dfd0df9c6
Frederik Krautwald <fkrautwald@gmail.com>
2018-03-06 00:36:25 +0100

    Update root dependencies
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

--- packages/test/src/collectEventsFor/collectEventsFor.ts
+++ packages/test/src/collectEventsFor/collectEventsFor.ts
@@ -2,7 +2,7 @@ import { Delay, Stream } from '@motorcycle/types'
 import { runEffects, tap } from '@motorcycle/stream'
 
 import { createTestScheduler } from '../createTestScheduler'
-import { curry2 } from '@most/prelude'
+import { curry2 } from '@typed/functions'
 
 /**
  * Collects events for a given amount of time.
@@ -11,14 +11,15 @@ import { curry2 } from '@most/prelude'
  * // Mocha style tests
  * it('increasing value by one', () => {
  *   const stream = scan(x => x + 1, skip(1, periodic(10)))
- * 
+ *
  *   return collectEventsFor(30, stream).then(events => assert.deepEqual(events, [0, 1, 2, 3]))
  * })
  */
-export const collectEventsFor: CollectEventsFor = curry2(function collectEventsFor<A>(
-  delay: Delay,
-  stream: Stream<A>
-) {
+export const collectEventsFor: CollectEventsFor = curry2<
+  Delay,
+  Stream<any>,
+  Promise<ReadonlyArray<any>>
+>(function collectEventsFor<A>(delay: Delay, stream: Stream<A>) {
   const { tick, scheduler } = createTestScheduler()
 
   const eventList: Array<A> = []
@@ -29,7 +30,7 @@ export const collectEventsFor: CollectEventsFor = curry2(function collectEventsF
 })
 
 export interface CollectEventsFor {
-  <A>(delay: Delay, stream: Stream<A>): Promise<ReadonlyArray<A>>
-  (delay: Delay): <A>(stream: Stream<A>) => Promise<ReadonlyArray<A>>
-  <A>(delay: Delay): (stream: Stream<A>) => Promise<ReadonlyArray<A>>
+  <A>(n: Delay, s: Stream<A>): Promise<ReadonlyArray<A>>
+  (n: Delay): <A>(s: Stream<A>) => Promise<ReadonlyArray<A>>
+  <A>(n: Delay): (s: Stream<A>) => Promise<ReadonlyArray<A>>
 }
